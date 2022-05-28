fc9fbdb072c99b4330f6058a84a8cd0716e24ca4
MMVahedi <m.m.vahedi13800@gmail.com>
2022-05-28 19:23:34 +0430

    Misit Game

---
+++ Misit Game/hfhjmbbdff.py
@@ -0,0 +1,1 @@
+import pygamepygame.init()pygame.mixer.init()pygame.mixer.music.load("music15.mp3")screen=pygame.display.set_mode((450,600))pygame.display.update()pygame.mixer.music.play()pygame.display.set_caption("MISIT")pygame.display.update()
